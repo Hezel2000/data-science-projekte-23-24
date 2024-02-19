a717b014baf38dc356d663b6a3884ef5b78b89c1
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2024-02-19 15:19:18 +0000

    almost there

--- Welcome.py
+++ Welcome.py
@@ -8,18 +8,21 @@ video_list = pd.read_csv('data/data-science-videos-23-24.csv')
 
 sel_project = st.selectbox('Wähle ein Projekt', video_list['Titel'])
 fil = video_list['Titel'] == sel_project
-vim_link = video_list[fil]['Vimeo_Link'][0]
+vim_link = video_list[fil]['Vimeo_Link'].tolist()[0]
 
 col1, col2 = st.columns([70,30])
-
 with col1:
     st_player(vim_link)
-
 with col2:
     with st.expander('Dauer', expanded=True):
-        st.write(video_list[fil]['Dauer'][0])
+        st.write(video_list[fil]['Dauer'].tolist()[0])
     with st.expander('Zusatz-Material', expanded=True):
-        st.link_button('Download', video_list[fil]['supplement'][0])
+        if video_list[fil]['supplement'].tolist()[0] != 'none available':
+            st.write("[Download](https://raw.githubusercontent.com/Hezel2000/Data_Science/main/jupyter_nb/Basics_Function_plotting_with_the_numpy_package.ipynb" + ")")
+        else:
+            'keines vorhanden'
+
+#st.link_button('Bewertungsseite', )
 
 #---------------------------------#
 #------ Main Page Sidebar --------#
